8f8639e949db5e5953f2a0f27b42b53e5086896f
Madhukesh <madhukesh04@gmail.com>
2020-07-21 22:19:04 +0530

    Day 17: Project 1 Part 2

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,23 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry","Ron","Hermoine"]
-    @State private var selectedStudent = 0
-    @State private var name = ""
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson:Double{
+        let peopleCount = Double(numberOfPeople+2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount/100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
     
     var body: some View {
-        VStack{
-            Picker("Select your student",selection: $selectedStudent){
-                ForEach(0..<students.count){
-                    Text(self.students[$0])
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People",selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header:Text("How much tips you want to give?")){
+                    Picker("Tips Percentage",selection: $tipPercentage){
+                        ForEach(0..<tipPercentages.count){
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())
+                }
+                Section{
+                    Text("₹\(totalPerPerson,specifier: "%.2f")")
                 }
             }
-            Text("You chose: \(students[selectedStudent])")
-            
-                Form{
-                    TextField("Enter your Name",text: $name)
-                    Text("Your name is \(name)")
-                }
+        .navigationBarTitle("WeSplit")
         }
     }
 }
