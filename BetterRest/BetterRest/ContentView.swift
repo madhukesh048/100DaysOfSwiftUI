2180a37d1775667db2f0f9085086eb91df538d43
Madhukesh <madhukesh04@gmail.com>
2020-08-01 22:01:31 +0530

    Day 28 - Project 4 Part 3

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -17,43 +17,56 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
+    let coffeeCups = [1,2,3,4,5,6,7,8,9,10,11,12]
+    
     var body: some View {
-        NavigationView {
-            VStack(spacing:20) {
-                Form {
-                    VStack(alignment:.leading, spacing: 0){
-                        Text("When do you want to wake up?")
-                                               .font(.headline)
-                        DatePicker("Please enter the Time",selection: $wakeUp,displayedComponents: .hourAndMinute).labelsHidden()
-                    }
-                    VStack(alignment:.leading, spacing: 0){
-                        Text("Desired amount of sleep")
-                        .font(.headline)
-                        Stepper(value:$sleepHours, in:4...12, step: 0.25){
-                            Text("\(sleepHours,specifier: "%g") Hours")
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.blue, .white, .pink]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            NavigationView {
+                    Form {
+                        Section{
+                            Text("When do you want to wake up?")
+                                                   .font(.headline)
+                            DatePicker("Please enter the Time",selection: $wakeUp,displayedComponents: .hourAndMinute).labelsHidden()
                         }
-                    }
-                    VStack(alignment:.leading, spacing: 0){
-                        Text("Daily coffee intake")
+                        Section{
+                            Text("Desired amount of sleep")
                             .font(.headline)
-                        Stepper(value:$coffeeAmount, in: 1...20){
-                            if coffeeAmount == 1{
-                                Text("1 cup")
-                            }else{
-                                Text("\(coffeeAmount) cups")
+                            Stepper(value:$sleepHours, in:4...12, step: 0.25){
+                                Text("\(sleepHours,specifier: "%g") Hours")
                             }
                         }
+                        Section{
+                            Text("Daily coffee intake")
+                                .font(.headline)
+                            Picker("\(coffeeCups[coffeeAmount]) cup/cups",selection: $coffeeAmount){
+                                ForEach(0..<coffeeCups.count){
+                                    Text("\(self.coffeeCups[$0])")
+                                }
+                            }
+    //                        Stepper(value:$coffeeAmount, in: 1...20){
+    //                            if coffeeAmount == 1{
+    //                                Text("1 cup")
+    //                            }else{
+    //                                Text("\(coffeeAmount) cups")
+    //                            }
+    //                        }
+                        }
+                        Section{
+                            Text("Your ideal bedtime is \(alertMessage)")
+                                .font(.custom("\(alertMessage)", size: 24))
+                                .foregroundColor(.blue)
+                        }
                     }
-                }
-            }
-            .navigationBarTitle("BetterRest")
-            .navigationBarItems(trailing:
-                Button(action: calculateBedtime) {
-                    Text("Calculate")
-                }
-            )
-            .alert(isPresented: $showingAlert) {
-                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                .navigationBarTitle("BetterRest")
+                .navigationBarItems(trailing:
+                    Button(action: calculateBedtime) {
+                        Text("Calculate")
+                    }
+                )
+//                .alert(isPresented: $showingAlert) {
+//                    Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+//                }
             }
         }
     }
