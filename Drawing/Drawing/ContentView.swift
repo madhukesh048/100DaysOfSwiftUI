bc37838f6b192c79a25b8c57fbe597a87029305c
Madhukesh <madhukesh04@gmail.com>
2020-08-22 22:01:19 +0530

    Day 44 - Project 9 Part 2

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -43,10 +43,95 @@ struct Arc: InsettableShape {
     }
 }
 
-struct ContentView: View {
+struct Flower: Shape {
+    // How much to move this petal away from the center
+    var petalOffset: Double = -20
+
+    // How wide to make each petal
+    var petalWidth: Double = 100
+
+    func path(in rect: CGRect) -> Path {
+        // The path that will hold all petals
+        var path = Path()
+
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+
+            // apply our rotation/position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+
+            // add it to our main path
+            path.addPath(rotatedPetal)
+        }
+
+        // now send the main path back
+        return path
+    }
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
     var body: some View {
-       Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-       .strokeBorder(Color.blue, lineWidth: 40)
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
+    
+    var body: some View {
+//       Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+//       .strokeBorder(Color.blue, lineWidth: 40)
+//        VStack {
+//                  Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+//                      .fill(Color.red, style: FillStyle(eoFill: true))
+//
+//                  Text("Offset")
+//                  Slider(value: $petalOffset, in: -40...40)
+//                      .padding([.horizontal, .bottom])
+//
+//                  Text("Width")
+//                  Slider(value: $petalWidth, in: 0...100)
+//                      .padding(.horizontal)
+//            }
+        VStack {
+                   ColorCyclingCircle(amount: self.colorCycle)
+                       .frame(width: 300, height: 300)
+
+                   Slider(value: $colorCycle)
+            }
     }
 }
 
