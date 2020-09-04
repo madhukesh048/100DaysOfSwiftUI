5f12b5234ed25de5904da96beb007ead2f0466f2
Madhukesh <madhukesh04@gmail.com>
2020-09-04 23:51:32 +0530

    Day 49 Project 10 Part 1

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Madhukesh on 04/09/20.
+//  Copyright © 2020 Madhukesh. All rights reserved.
+//
+
+import SwiftUI
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+struct ContentView: View {
+    @State private var results = [Result]()
+    
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
+    var body: some View {
+        Form {
+               Section {
+                   TextField("Username", text: $username)
+                   TextField("Email", text: $email)
+               }
+
+               Section {
+                   Button("Create account") {
+                       print("Creating account…")
+                   }
+               }
+               .disabled(disableForm)
+
+           }
+//        List(results, id: \.trackId) { item in
+//            VStack(alignment: .leading) {
+//                Text(item.trackName)
+//                    .font(.headline)
+//                Text(item.collectionName)
+//            }
+//        }
+//        .onAppear(perform: loadData)
+    }
+//    func loadData() {
+//        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+//            print("Invalid URL")
+//            return
+//        }
+//        let request = URLRequest(url: url)
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data {
+//                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+//                    // we have good data – go back to the main thread
+//                    DispatchQueue.main.async {
+//                        // update our UI
+//                        self.results = decodedResponse.results
+//                    }
+//
+//                    // everything is good, so we can exit
+//                    return
+//                }
+//            }
+//
+//            // if we're still here it means there was a problem
+//            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+//        }.resume()
+//    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
