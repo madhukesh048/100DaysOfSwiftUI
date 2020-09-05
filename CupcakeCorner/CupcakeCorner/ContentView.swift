2f3c73493b937185cb60a2a7d1beb75ec1ea0d3e
Madhukesh <madhukesh04@gmail.com>
2020-09-05 23:18:03 +0530

    Day 50 - Project 10 Part 2

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,74 +8,46 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
-    
-    @State private var username = ""
-    @State private var email = ""
-    
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
-    }
-    
+    @ObservedObject var order = Order()
     var body: some View {
-        Form {
-               Section {
-                   TextField("Username", text: $username)
-                   TextField("Email", text: $email)
-               }
+          NavigationView {
+              Form {
+                  Section {
+                      Picker("Select your cake type", selection: $order.type) {
+                          ForEach(0..<Order.types.count,id: \.self) {
+                              Text(Order.types[$0])
+                          }
+                      }
 
-               Section {
-                   Button("Create account") {
-                       print("Creating account…")
-                   }
-               }
-               .disabled(disableForm)
+                      Stepper(value: $order.quantity, in: 3...20) {
+                          Text("Number of cakes: \(order.quantity)")
+                      }
+                  }
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("Any special requests?")
+                    }
 
-           }
-//        List(results, id: \.trackId) { item in
-//            VStack(alignment: .leading) {
-//                Text(item.trackName)
-//                    .font(.headline)
-//                Text(item.collectionName)
-//            }
-//        }
-//        .onAppear(perform: loadData)
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting) {
+                            Text("Add extra frosting")
+                        }
+
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("Add extra sprinkles")
+                        }
+                    }
+                }
+                Section{
+                    NavigationLink(destination:AdressView(order: order)){
+                        Text("Delivery Details")
+                    }
+                }
+              }
+              .navigationBarTitle("Cupcake Corner")
+          }
     }
-//    func loadData() {
-//        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-//            print("Invalid URL")
-//            return
-//        }
-//        let request = URLRequest(url: url)
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//            if let data = data {
-//                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-//                    // we have good data – go back to the main thread
-//                    DispatchQueue.main.async {
-//                        // update our UI
-//                        self.results = decodedResponse.results
-//                    }
-//
-//                    // everything is good, so we can exit
-//                    return
-//                }
-//            }
-//
-//            // if we're still here it means there was a problem
-//            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-//        }.resume()
-//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
